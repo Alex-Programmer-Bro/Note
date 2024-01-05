f70b996eb9255892d5010043946ed799e6dd48a4
Alex-Programer <alex.lichangnan@gmail.com>
2024-01-05 17:32:23 +0800

    test: define test name for some cases

--- cypress/e2e/note.cy.js
+++ cypress/e2e/note.cy.js
@@ -1,20 +1,19 @@
-describe('note', () => {
+describe('manage note', () => {
   beforeEach(() => {
     cy.visit('../../playground/index.html');
   });
 
-  it('selected a word', () => {
+  it('Successfully create a note', () => {
     cy.get('.ce-paragraph.cdx-block').focus().type('hello note');
     cy.selectText('note');
     cy.get('[data-tool="Note"]').click();
     cy.get('note').should('be.visible').click();
     cy.expectStyle('note', { borderBottom: '1px solid rgb(221, 221, 221)' });
-    
     cy.reloadPL();
     cy.get('note').should('be.visible').should('have.text', 'note').click();
   });
 
-  it('nested selecte', () => {
+  it('Successfully create nested note', () => {
     cy.get('.ce-paragraph.cdx-block').focus().type('hello world');
     cy.selectText('world');
     cy.get('[data-tool="Note"]').click();
@@ -22,7 +21,7 @@ describe('note', () => {
     cy.selectText('hello world');
     cy.get('[data-tool="Note"]').should('have.not.class', 'ce-inline-tool--active').click();
     cy.get('body').find('note').its('length').should('eq', 2);
-    cy.get('note[note-id]').should('be.visible').should('have.text', 'world');
+    cy.get('note[note-id]').should('be.visible').should('have.text', 'world').click();
     cy.expectStyle('note[note-id]', { backgroundColor: 'rgb(221, 221, 221)' });
   })
 });
